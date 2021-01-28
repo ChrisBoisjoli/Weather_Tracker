e0c42867f58f249fa90aeb8cdb85b1cff68ecd1b
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-01-27 18:35:14 -0600

    updated script to get necessary information

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,29 @@
 // ajax call for pulling weather
+// full call link
+// api.openweathermap.org/data/2.5/weather?q=minneapolis&appid=9ac0788da70afdca53d6b9c82e029618
 
 var city = "";
 
 function searchWeather(city){
     var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=9ac0788da70afdca53d6b9c82e029618";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+
+        var tBody = $("tbody");
+
+        var tRow = $("<tr>");
+        var cityName = $("<td>").text(response.name);
+        var temperature = $("<td>").text(response.main.temp);
+        var humidity = $("<td>").text(response.main.humidity);
+        var windspeed = $("<td>").text(response.Actors);
+        var uvIndex = $("<td>").text(response.main.temp);
+// temp 
+// humidity
+// windspeed
+// UV index
+    };
 };
+
