95d6d7d1ff713b26dbf49bb3fcbee1d41dfbe1d9
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-01-27 19:10:04 -0600

    corrected queryURL

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@
 var city = "";
 
 function searchWeather(city){
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=9ac0788da70afdca53d6b9c82e029618";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=9ac0788da70afdca53d6b9c82e029618";
 
     $.ajax({
         url: queryURL,
