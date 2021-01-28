18f74899fa29de4817aee0e07ecdbab56496e71f
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-01-27 18:17:42 -0600

    add code for api and change zipcode to city

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// ajax call for pulling weather
+
+var city = "";
+
+function searchWeather(city){
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=9ac0788da70afdca53d6b9c82e029618";
+};
