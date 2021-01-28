48734edb02028dd7641be44d7ceda4df5823e124
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-01-27 19:48:12 -0600

    updated to only allow 2 decimal places in temperature

--- assets/script.js
+++ assets/script.js
@@ -16,10 +16,18 @@ function searchWeather(city){
 
         var tRow = $("<tr>");
         var cityName = $("<td>").text(response.name);
-        var temperature = $("<td>").text(response.main.temp);
-        var humidity = $("<td>").text(response.main.humidity);
-        var windspeed = $("<td>").text(response.wind.speed);
 
+        var tempK = response.main.temp;
+        var tempFa= (tempK - 273.15) * 1.80 + 32;
+        console.log(tempF);   
+        var tempF = tempFa.toFixed(2);
+        var temperature = $("<td>").text(tempF + " &deg, F").html(tempF + "&deg;F");
+        
+        var humidity = $("<td>").text(response.main.humidity + " %");
+        var windspeed = $("<td>").text(response.wind.speed + " meter/sec");
+
+        
+        
         tRow.append(cityName);
         tRow.append(temperature);
         tRow.append(humidity);
@@ -30,6 +38,10 @@ function searchWeather(city){
 
     });
 };
+function temperatureConverter(temperature) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
+  }
 searchWeather("minneapolis");
 
 // UV Index API CALL http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
